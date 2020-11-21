82efba98a95e2ff3102aa7bc4417d8367a0770dc
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2020-11-21 02:30:30 -0300

    hdl: Adding a version control file #1

---
+++ hdl/version.vhd
@@ -0,0 +1,20 @@
+--! 
+--! \brief Version control file.
+--! 
+--! \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+--! 
+--! \version 0.0.0
+--! 
+--! \date 2020/11/21
+--! 
+
+package version is
+
+    constant GRISC_VERSION  : string := "0.0.0";
+
+    constant GRISC_AUTHOR   : string := "Gabriel Mariano Marcelino";
+    constant GRISC_EMAIL    : string := "gabriel.mm8@gmail.com";
+
+    constant GRISC_STATUS   : string := "Development";
+
+end version;
