06d4f55facb491eef3d83be8107941250926ce9f
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2020-11-25 23:58:27 -0300

    Defining the license as SHL-2.1 #2

--- hdl/reg.vhd
+++ hdl/reg.vhd
@@ -1,9 +1,32 @@
+--
+-- reg.vhd
+--
+-- Copyright (C) 2020, Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+--
+-- SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+--
+-- Licensed under the Solderpad Hardware License v 2.1 (the "License");
+-- you may not use this file except in compliance with the License,
+-- or, at your option, the Apache License version 2.0.
+--
+-- You may obtain a copy of the License at
+--
+-- https://solderpad.org/licenses/SHL-2.1/
+--
+-- Unless required by applicable law or agreed to in writing, any
+-- work distributed under the License is distributed on an "AS IS"
+-- BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+-- express or implied. See the License for the specific language
+-- governing permissions and limitations under the License.
+--
+--
+
 --! 
 --! \brief Flip-flop register.
 --! 
 --! \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
 --! 
---! \version 0.0.10
+--! \version 0.0.15
 --! 
 --! \date 2020/11/21
 --! 
@@ -19,21 +42,25 @@ entity Reg is
         clk         : in std_logic;                                 --! Clock input.
         rst         : in std_logic;                                 --! Reset signal.
         input       : in std_logic_vector(DATA_WIDTH-1 downto 0);   --! Data input.
-        output      : out std_logic_vector(DATA_WIDTH-1 downto 0);  --! Data output.
+        output      : out std_logic_vector(DATA_WIDTH-1 downto 0)   --! Data output.
         );
 end Reg;
 
 architecture behavior of Reg is
 
+    signal output_sig : std_logic_vector(DATA_WIDTH-1 downto 0) := (others => '0');
+
 begin
 
     process(clk)
     begin
-        if rst = '1' then
-            output <= (others => 0);
+        if rst = '0' then
+            output_sig <= (others => '0');
         elsif rising_edge(clk) then
-            output <= input;
+            output_sig <= input;
         end if;
     end process;
+
+    output <= output_sig;
 
 end behavior;
